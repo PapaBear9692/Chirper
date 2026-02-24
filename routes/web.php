6f076b68ebc0c1aab11e03692942f36adb772d89
PapaBear <nishatislam84@gmail.com>
2026-02-24 10:44:38 +0600

    added home route and home page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function(){
-    return view('hello');
+Route::get('/', function(){
+    return view('home');
 });
