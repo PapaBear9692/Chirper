42085bd8327bc249f021771780d68d1d5974e4f3
PapaBear <nishatislam84@gmail.com>
2026-02-24 13:43:05 +0600

    deleted old ChirpController, created a new with the --resource parameter

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
-    //Controller to route and coordinate chirp related requests
-    
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $chirps = [
@@ -28,5 +30,53 @@ class ChirpController extends Controller
         ];
 
         return view('home', ['data' => $chirps]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
