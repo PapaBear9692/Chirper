d29f7cb72229cd439a31e273786a13c14bbe3c9c
PapaBear <nishatislam84@gmail.com>
2026-02-24 12:33:02 +0600

    updated chirpcontroller to send data, updated hom to receive data, updated route to use chirpcontroller

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
     //Controller to route and coordinate chirp related requests
+    
     public function index()
     {
-        return view('home');
+        $chirps = [
+            [
+                'author' => 'Jane Doe',
+                'message' => 'Just deployed my first Laravel app! 🚀',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'John Smith',
+                'message' => 'Laravel makes web development fun again!',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Alice Johnson',
+                'message' => 'Working on something cool with Chirper...',
+                'time' => '3 hours ago'
+            ]
+        ];
+
+        return view('home', ['data' => $chirps]);
     }
 }
