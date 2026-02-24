5f57eb4ccf856bf44d7063663e1a742b16fc299e
PapaBear <nishatislam84@gmail.com>
2026-02-24 14:32:17 +0600

    added migration chirps and added 1 data entry using artisan tinker table()->insert()

--- database/migrations/2026_02_24_082954_create_chirps_table.php
+++ database/migrations/2026_02_24_082954_create_chirps_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chirp', function (Blueprint $table) {
+        Schema::create('chirps', function (Blueprint $table) {
             $table->id();
             $table->foreignId(('user_id'))->nullable()->constrained()->cascadeOnDelete();
             $table->string('message', 255);
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chirp');
+        Schema::dropIfExists('chirps');
     }
 };
